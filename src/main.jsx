37b1087effe42854449b25e164631da646511b9b
Vaishnavi Kumbhar <kvaish1999u@gmail.com>
2026-08-18 14:57:05 +0530

    Fix GitHub Pages routing and paths

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/priyankas-creation">
       <AuthProvider>
         <ShopProvider>
           <App />
